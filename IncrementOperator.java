b744751d941057c1c0103211a56910d56a11d89f
Ygor Nascimento <ygo.nascimento@outlook.com>
2025-09-24 05:37:16 -0300

    2.1.8 - Incrementing and Decrementing Variables.

---
+++ IncrementOperator.java
@@ -0,0 +1,7 @@
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int homeGrade = 0;
+        System.out.println(++homeGrade);
+        System.out.println(homeGrade);
+    }
+}
